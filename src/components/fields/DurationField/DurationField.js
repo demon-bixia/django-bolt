4c3df58ee1f1f5933632a6860c71f7d833620308
muhammad <msmainacc0unt@gmail.com>
2022-05-29 11:27:18 +0200

    add DurationField.js

---
+++ src/components/fields/DurationField/DurationField.js
@@ -0,0 +1,49 @@
+import TextField from '@mui/material/TextField';
+import {AdapterDateFns} from '@mui/x-date-pickers/AdapterDateFns';
+import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
+import {DateTimePicker} from '@mui/x-date-pickers/DateTimePicker';
+import FeatherIcon from "feather-icons-react";
+import moment from "moment";
+
+const OpenIcon = () => {
+    return (
+        <FeatherIcon icon="calendar" size={20}/>
+    );
+};
+
+// the form input only accepts one format
+const DurationField = ({
+                           field: {attrs, type},
+                           value,
+                           dateInputValue,
+                           errors,
+                           inputProps,
+                           onClick,
+                           onChange,
+                           ...props
+                       }) => {
+
+    return (
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <DateTimePicker
+                required={inputProps['required'] || false}
+                components={{OpenPickerIcon: OpenIcon}}
+                label={attrs['label']}
+                value={dateInputValue || ''}
+                onChange={event => onChange({
+                    'formatted': moment(event).format('DD HH:MM:SS'),
+                    'default': event
+                }, props.name, 'dateTimePicker')}
+                renderInput={(params) => <TextField
+                    {...params}
+                    {...props}
+                    error={!!errors}
+                    onClick={event => onClick(event, props.name)}
+                    helperText={errors ? errors.join('.\n') : attrs['help_text'] || ''}
+                />}
+            />
+        </LocalizationProvider>
+    );
+};
+
+export default DurationField;
