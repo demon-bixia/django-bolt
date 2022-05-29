6545e61f572daf258a2bf891b518ec4f2f71848f
muhammad <msmainacc0unt@gmail.com>
2022-05-29 11:34:47 +0200

    add favicon.ico

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import {ThemeProvider} from '@mui/material/styles';
+import {overrides} from "./theme";
+import CssBaseline from "@mui/material/CssBaseline";
+import DynamicForm from "./components/DynamicForm/DynamicForm";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <ThemeProvider theme={overrides}>
+                <CssBaseline/>
+                <DynamicForm/>
+            </ThemeProvider>
+        </div>
+    );
 }
 
 export default App;
